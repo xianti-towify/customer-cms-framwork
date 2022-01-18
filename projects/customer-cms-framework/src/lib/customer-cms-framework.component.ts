0e1ea85cf60e0c271136f07c62b19e05cd432e89
xianti-towify <xianti.xiong@towify.com>
2022-01-18 13:54:47 +0800

    add config file

--- projects/customer-cms-framework/src/lib/customer-cms-framework.component.ts
+++ projects/customer-cms-framework/src/lib/customer-cms-framework.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'customer-cms-framework',
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['customer-cms-framework.component.scss']
 })
 export class CustomerCmsFrameworkComponent implements OnInit {
+  @Input()
+  style: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
